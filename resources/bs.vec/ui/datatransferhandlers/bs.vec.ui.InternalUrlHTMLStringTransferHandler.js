84ce09065577d46f506bde81d0980f76f87f56ca
Peter Boehm <boehm@hallowelt.com>
2019-04-02 14:51:57 +0200

    Enable Pasting of Text with embedded Links
    With this Change pasting of text with embedded links
    into Visual Editor will change in behaviour:
    
    On pasting plain text http and https links will no more be surrounded with
    nowiki tags, but will be displayed as external links.
    On pasting plain text every linebreak will cause a paragraph, so new lines and
    double lines will work.
    Using wgUrlProtocols for this.
    
    For pasting from browser and/or office the contained links are kept
    
    Cherrypick to REL1_31
    ERM#14510
    
    Change-Id: I7d400686a25d7280f46c54b76318441a98cfa219

---
+++ resources/bs.vec/ui/datatransferhandlers/bs.vec.ui.InternalUrlHTMLStringTransferHandler.js
@@ -0,0 +1,71 @@
+/*!
+ * BlueSpiceVisualEditorConnector UserInterface InternalUrlHTMLStringTransferHandler class.
+ *
+ * Heavily inspired by VisualEditor UserInterface UrlStringTransferHandler class.
+ *
+ * @copyright 2019 Hallo Welt! GmbH
+ */
+
+/**
+ * Handle pastes of html code containing links.
+ *
+ * @class
+ * @extends bs.vec.ui.InternalUrlHTMLStringTransferHandler
+ *
+ * @constructor
+ * @param {ve.ui.Surface} surface
+ * @param {ve.ui.DataTransferItem} item
+ */
+bs.vec.ui.InternalUrlHTMLStringTransferHandler = function BsVecUiInternalUrlHTMLStringTransferHandler() {
+	// Parent constructor
+	bs.vec.ui.InternalUrlHTMLStringTransferHandler.super.apply( this, arguments );
+};
+
+/* Inheritance */
+
+OO.inheritClass( bs.vec.ui.InternalUrlHTMLStringTransferHandler, ve.ui.HTMLStringTransferHandler );
+
+/* Static properties */
+bs.vec.ui.InternalUrlHTMLStringTransferHandler.static.name = 'htmlString';
+
+bs.vec.ui.InternalUrlHTMLStringTransferHandler.static.types = ['text/html', 'application/xhtml+xml'];
+
+bs.vec.ui.InternalUrlHTMLStringTransferHandler.static.handlesPaste = true;
+
+/**
+ * RegExp matching an external url.
+ * @property {RegExp}
+ * @private
+ */
+bs.vec.ui.InternalUrlHTMLStringTransferHandler.static.urlRegExp = null; // Initialized below
+
+ve.init.Platform.static.initializedPromise.then( function () {} );
+
+/**
+ * @inheritdoc
+ */
+bs.vec.ui.InternalUrlHTMLStringTransferHandler.prototype.process = function () {
+	let newRules = {
+		external: {
+			blacklist: [
+				// Annotations
+				'textStyle/span', 'textStyle/font', 'textStyle/underline', 'meta/language', 'textStyle/datetime',
+				// Nodes
+				'article', 'section', 'div', 'alienInline', 'alienBlock', 'comment'
+			],
+			htmlBlacklist: {
+				remove: ['sup.reference:not( [typeof] )'],
+				unwrap: ['fieldset', 'legend']
+			},
+			removeOriginalDomElements: false,
+			nodeSanitization: false
+		},
+		all: null
+	};
+	let doc = this.surface.getModel().getDocument().newFromHtml( this.item.getAsString(), newRules );
+	this.resolve( doc );
+
+};
+
+/* Registration */
+ve.ui.dataTransferHandlerFactory.register( bs.vec.ui.InternalUrlHTMLStringTransferHandler );
---
+++ resources/bs.vec/ui/datatransferhandlers/bs.vec.ui.InternalUrlPlainTextStringTransferHandler.js
@@ -0,0 +1,110 @@
+/*!
+ * BlueSpiceVisualEditorConnector UserInterface InternalUrlPlainTextStringTransferHandler class.
+ *
+ * Heavily inspired by VisualEditor UserInterface UrlStringTransferHandler class.
+ *
+ * @copyright 2019 Hallo Welt! GmbH
+ */
+
+/**
+ * Handle pastes of plain text containing links.
+ * Attempts to preserve link titles when possible.
+ *
+ * @class
+ * @extends bs.vec.ui.InternalUrlPlainTextStringTransferHandler
+ *
+ * @constructor
+ * @param {ve.ui.Surface} surface
+ * @param {ve.ui.DataTransferItem} item
+ */
+bs.vec.ui.InternalUrlPlainTextStringTransferHandler = function BsVecUiInternalUrlPlainTextStringTransferHandler() {
+	// Parent constructor
+	bs.vec.ui.InternalUrlPlainTextStringTransferHandler.super.apply( this, arguments );
+};
+
+/* Inheritance */
+OO.inheritClass( bs.vec.ui.InternalUrlPlainTextStringTransferHandler, ve.ui.PlainTextStringTransferHandler );
+
+/* Static properties */
+bs.vec.ui.InternalUrlPlainTextStringTransferHandler.static.name = 'internalString';
+
+bs.vec.ui.InternalUrlPlainTextStringTransferHandler.static.handlesPaste = true;
+
+/**
+ * @param {string} text
+ * @returns {unresolved}
+ */
+bs.vec.ui.InternalUrlPlainTextStringTransferHandler.urlify = function ( text ) {
+	var urlRegex = new RegExp( ve.init.platform.getExternalLinkUrlProtocolsRegExp().source.slice( 1 )
+			+ '(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])', 'gi' );
+
+	return text.replace( urlRegex, function ( url ) {
+		return ' _mark_ start_ ' + url + ' _mark_ end_ ';
+	} );
+
+};
+
+/**
+ * @inheritdoc
+ * DANGER - Magic is happening here, modify at own risk
+ * In the data a magic word ' _mark_ ' is used as separator to find links in pasted text.
+ * This magic word is first replaced by another magic word, in case it is already used in the data
+ */
+bs.vec.ui.InternalUrlPlainTextStringTransferHandler.prototype.process = function () {
+	var links, html, doc, result,
+			surface = this.surface,
+			store = surface.getModel().getDocument().getStore(),
+			linkAction = ve.ui.actionFactory.create( 'link', surface ),
+			data = this.item.getAsString() + ' ';
+	data = " _mark_ " + data.split( " _mark_ " ).join( " _mark_ mark_ " );
+	data = bs.vec.ui.InternalUrlPlainTextStringTransferHandler.urlify( data );
+	let fields = data.split( ' _mark_ ' );
+
+	result = [];
+	let endflag = false; // to fix newline after annotated link
+	result.push( {type: 'paragraph', internal: {generated: 'wrapper'}} );
+	fields.forEach( function ( element ) {
+		if ( element.startsWith( "start_ " ) ) {
+			element = element.slice( 7 );
+			let annotation = linkAction.getLinkAnnotation( element );
+			let annotationSet = new ve.dm.AnnotationSet( store, store.hashAll( [
+				annotation
+			] ) );
+			let title = annotation.getDisplayTitle();
+			let content = title.split( '' );
+			ve.dm.Document.static.addAnnotationsToData( content, annotationSet );
+			for ( let i = 0; i < content.length; i++ ) {
+				result.push( content[i] );
+			}
+		} else {
+			if ( element.startsWith( "end_ " ) ) {
+				endflag = true;
+				element = element.slice( 5 );
+			} else {
+				endflag = false;
+				if ( element.startsWith( "mark_ " ) ) {
+					//this is the case when ' _mark_ ' was used in copied text
+					element = ' _' + element;
+				}
+			}
+			let content = element.split( '' );
+			for ( let i = 0; i < content.length; i++ ) {
+				if ( content[ i ] === "\n" ) {
+					if ( endflag ) {
+						result.push( ' ' );
+					}
+					result.push( {type: '/paragraph'} );
+					result.push( {type: 'paragraph', internal: {generated: 'wrapper'}} );
+				} else {
+					result.push( content[ i ] );
+				}
+				endflag = false;
+			}
+		}
+	} );
+	result.push( {type: '/paragraph'} );
+	this.resolve( result );
+};
+
+/* Registration */
+ve.ui.dataTransferHandlerFactory.register( bs.vec.ui.InternalUrlPlainTextStringTransferHandler );
