14bbe42d1492eaef4095acaec30bdc17b0f48560
Markus Glaser <glaser@hallowelt.com>
2019-05-23 09:06:42 +0200

    Prevent cell editing in TableSelection mode in IE11
    When editing a cell in TableSelection mode, the text is scrumbled. This
    commit disables modifications. It is a workaround for BlueSpice until a
    proper solution is found in VisualEditor
    
    Needs cherry-pick to REL1_31 and master
    
    Bug: T220984
    ERM#14844
    
    Change-Id: I80eb7d7dc42910630bff5675fc42f68faaf20e47

---
+++ resources/ve-visualeditorconnector/ve.init.bs.PreventCellEditInTableSelectionInIE11.js
@@ -0,0 +1,30 @@
+/* This modification prevents table cell editing with a single click in IE11.
+ * When editing with single click (aka in TableSelection), IE moves the characters
+ * from front to back.
+ *
+ * Bug: T220984 ERM#14844
+ */
+
+ve.ce.Surface.prototype._onDocumentKeyPress = ve.ce.Surface.prototype.onDocumentKeyPress;
+
+ve.ce.Surface.prototype.onDocumentKeyPress = function ( e ) {
+	if (
+		this.model.selection instanceof ve.dm.TableSelection &&
+		$.client.profile().name === 'msie'
+	) {
+		return;
+	}
+	return this._onDocumentKeyPress( e );
+}
+
+ve.ce.Surface.prototype._onPaste = ve.ce.Surface.prototype.onPaste;
+
+ve.ce.Surface.prototype.onPaste = function ( e ) {
+	if (
+		this.model.selection instanceof ve.dm.TableSelection &&
+		$.client.profile().name === 'msie'
+	) {
+		return;
+	}
+	return this._onPaste( e );
+}
