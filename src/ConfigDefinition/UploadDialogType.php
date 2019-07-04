d8f6839fef1d2d7f8da23bb5349d5f565c318355
rvogel <vogel@hallowelt.biz>
2019-07-04 16:41:26 +0200

    Add config definitions
    for `SimpleSaveProcess` and `UploadDialogType`.
    
    NEEDS CHERRY-PICK TO `master`!
    
    Change-Id: Id9287227a16dbd80cae8bae754cd4185d43036f2

---
+++ src/ConfigDefinition/UploadDialogType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace BlueSpice\VisualEditorConnector\ConfigDefinition;
+
+use BlueSpice\ConfigDefinition;
+
+use HTMLSelectField;
+
+class UploadDialogType extends ConfigDefinition {
+
+	/**
+	 *
+	 * @return array
+	 */
+	public function getPaths() {
+		return [
+			static::MAIN_PATH_FEATURE . '/' . static::FEATURE_EDITOR . '/BlueSpiceVisualEditorConnector',
+			static::MAIN_PATH_EXTENSION . '/BlueSpiceVisualEditorConnector/' . static::FEATURE_EDITOR ,
+			static::MAIN_PATH_PACKAGE . '/' . static::PACKAGE_FREE . '/BlueSpiceVisualEditorConnector',
+		];
+	}
+
+	/**
+	 * @return \HTMLFormField
+	 */
+	public function getHtmlFormField() {
+		return new HTMLSelectField( $this->makeFormFieldParams() );
+	}
+
+	protected function makeFormFieldParams() {
+		$params = parent::makeFormFieldParams();
+		$params['options-messages'] = [
+			'bs-visualeditorconnector-upload-type-original' => 'original',
+			'bs-visualeditorconnector-upload-type-simple' => 'simple',
+			'bs-visualeditorconnector-upload-type-one-click' => 'one-click',
+		];
+		$params['options'] = [
+			'original' => 'original',
+			'simple' => 'simple',
+			'one-click' => 'one-click',
+		];
+		return $params;
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getLabelMessageKey() {
+		return 'bs-visualeditorconnector-upload-type';
+	}
+
+}
