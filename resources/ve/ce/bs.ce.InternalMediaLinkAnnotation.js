0703fba0e0414e835e07957908ff095c2c19b2a6
oleksandrpinchuk <intracomof@gmail.com>
2019-09-11 15:28:46 +0300

    Handle MediaLinks
    ERM 16201
    
    Change-Id: Ief688e6b5108631d2e010a93adeea265b470ef31

---
+++ resources/ve/ce/bs.ce.InternalMediaLinkAnnotation.js
@@ -0,0 +1,17 @@
+bs.util.registerNamespace( 'bs.vec.ce' );
+
+bs.vec.ce.InternalMediaLinkAnnotation = function() {
+	// Parent InternalMediaLinkAnnotation
+	bs.vec.ce.InternalMediaLinkAnnotation.super.apply( this, arguments );
+};
+
+/* Inheritance */
+
+OO.inheritClass( bs.vec.ce.InternalMediaLinkAnnotation, ve.ce.MWInternalLinkAnnotation );
+
+/* Static Properties */
+
+bs.vec.ce.InternalMediaLinkAnnotation.static.name = 'link/internalMedia';
+
+/* Registration */
+ve.ce.annotationFactory.register( bs.vec.ce.InternalMediaLinkAnnotation );
