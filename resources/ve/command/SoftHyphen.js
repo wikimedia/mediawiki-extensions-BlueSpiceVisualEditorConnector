0e48c49a57db2472493cadc4c55399da762ee037
Dejan Savuljesku <d.savuljesku@gmail.com>
2020-01-24 14:42:16 +0100

    Add soft hyphen functionality
    [3.1.x]
    
    ERM 17466
    
    Change-Id: I16b816d961e4f420ccc9ce60260e3df3e602bfa5

---
+++ resources/ve/command/SoftHyphen.js
@@ -0,0 +1,37 @@
+bs.util.registerNamespace( 'bs.vec.command' );
+
+bs.vec.command.SoftHyphen = function() {
+	// Parent constructor
+	bs.vec.command.SoftHyphen.super.call(
+		this, 'bsInsertSoftHyphen'
+	);
+	this.warning = null;
+	this.supportedSelections = ['linear'];
+	this.action = 'content';
+	this.method = 'insert';
+};
+
+/* Inheritance */
+
+OO.inheritClass( bs.vec.command.SoftHyphen, ve.ui.Command );
+
+/* Methods */
+
+/**
+ * @inheritdoc
+ */
+bs.vec.command.SoftHyphen.prototype.execute = function () {
+	var surfaceModel = ve.init.target.getSurface().getModel();
+	surfaceModel.getFragment()
+		.adjustLinearSelection( 1 )
+		.collapseToStart()
+		.insertContent( [
+			// \uOOAD => &shy;
+			{ type: 'softHyphen', attributes: { character: '\u00AD' } }
+		] );
+
+	return true;
+};
+
+/* Registration */
+ve.ui.commandRegistry.register( new bs.vec.command.SoftHyphen() );
