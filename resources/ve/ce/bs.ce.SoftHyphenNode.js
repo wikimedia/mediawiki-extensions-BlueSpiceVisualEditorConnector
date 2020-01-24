c3f0e9df6c7bfce22f4ceabd72a78e3333028618
Dejan Savuljesku <d.savuljesku@gmail.com>
2020-01-24 14:42:16 +0100

    Add soft hyphen functionality
    [3.1.x]
    
    ERM 17466
    
    Change-Id: I16b816d961e4f420ccc9ce60260e3df3e602bfa5

---
+++ resources/ve/ce/bs.ce.SoftHyphenNode.js
@@ -0,0 +1,19 @@
+bs.util.registerNamespace( 'bs.vec.ce' );
+
+bs.vec.ce.SoftHyphenNode = function() {
+	// Parent constructor
+	bs.vec.ce.SoftHyphenNode.super.apply( this, arguments );
+
+	this.$element.addClass( 'bs-soft-hyphen-node' );
+};
+
+/* Inheritance */
+OO.inheritClass( bs.vec.ce.SoftHyphenNode, ve.ce.MWEntityNode );
+
+/* Static Properties */
+
+bs.vec.ce.SoftHyphenNode.static.name = 'softHyphen';
+
+/* Registration */
+
+ve.ce.nodeFactory.register( bs.vec.ce.SoftHyphenNode );
---
+++ resources/ve/dm/bs.dm.SoftHyphenNode.js
@@ -0,0 +1,16 @@
+bs.util.registerNamespace( 'bs.vec.dm' );
+
+bs.vec.dm.SoftHyphenNode = function () {
+	// Parent constructor
+	bs.vec.dm.SoftHyphenNode .super.apply( this, arguments );
+};
+
+/* Inheritance */
+OO.inheritClass( bs.vec.dm.SoftHyphenNode, ve.dm.MWEntityNode );
+
+/* Static Properties */
+bs.vec.dm.SoftHyphenNode.static.name = 'softHyphen';
+
+
+/* Registration */
+ve.dm.modelRegistry.register( bs.vec.dm.SoftHyphenNode  );
