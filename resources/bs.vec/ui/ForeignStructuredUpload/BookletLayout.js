322a8fd9d8a0663614e409d8762d1ee525202fa4
Dejan Savuljesku <d.savuljesku@gmail.com>
2020-04-30 12:56:19 +0200

    Allow re-uploading deleted files and overwritting files from VisualEditor
    This is a lot of overriding, so by nature not nice, but its the only way
    
    [3.1.x]
    
    ERM 18757
    
    Change-Id: I7b797020c2d6cf118e88096bdbd7e6ef70236af5

--- resources/bs.vec/ui/ForeignStructuredUpload/BookletLayout.js
+++ resources/bs.vec/ui/ForeignStructuredUpload/BookletLayout.js
@@ -2,6 +2,8 @@ bs.util.registerNamespace( 'bs.vec.ui.ForeignStructuredUpload' );
 
 bs.vec.ui.ForeignStructuredUpload.BookletLayout = function BsVecUiForeignStructuredUploadBookletLayout( config ) {
 	bs.vec.ui.ForeignStructuredUpload.BookletLayout.parent.call( this, config );
+
+	this.saveCalledInit = true;
 };
 
 OO.inheritClass( bs.vec.ui.ForeignStructuredUpload.BookletLayout, mw.ForeignStructuredUpload.BookletLayout );
@@ -15,4 +17,75 @@ bs.vec.ui.ForeignStructuredUpload.BookletLayout.prototype.getFilename = function
 	this.emit( 'getfilename', this, data );
 
 	return data.filename;
+};
+
+bs.vec.ui.ForeignStructuredUpload.BookletLayout.prototype.createUpload = function () {
+	return new bs.vec.ui.ForeignStructuredUpload.ForeignStructuredUpload( this.target, {
+		parameters: {
+			errorformat: 'html',
+			errorlang: mw.config.get( 'wgUserLanguage' ),
+			errorsuselocal: 1,
+			formatversion: 2
+		}
+	} );
+};
+
+bs.vec.ui.ForeignStructuredUpload.BookletLayout.prototype.saveFile = function() {
+	if ( this.saveCalledInit === false ) {
+		this.uploadPromise = this.upload.uploadToStash( true );
+	}
+	var layout = this,
+		deferred = $.Deferred();
+
+	this.upload.setFilename( this.getFilename() );
+	this.upload.setText( this.getText() );
+
+	this.uploadPromise.then( function () {
+		layout.upload.finishStashUpload( {
+			ignorewarnings: layout.saveCalledInit === false
+		} ).then( function () {
+			var name;
+
+			// Normalize page name and localise the 'File:' prefix
+			name = new mw.Title( 'File:' + layout.upload.getFilename() ).toString();
+			layout.filenameUsageWidget.setValue( '[[' + name + ']]' );
+			layout.setPage( 'insert' );
+
+			deferred.resolve();
+			layout.emit( 'fileSaved', layout.upload.getImageInfo() );
+		}, function () {
+			layout.getErrorMessageForStateDetails().then( function ( errorMessage ) {
+				deferred.reject( errorMessage );
+			} );
+		} );
+		layout.saveCalledInit = false;
+	} );
+
+	return deferred.promise();
+};
+
+bs.vec.ui.ForeignStructuredUpload.BookletLayout.prototype.getErrorMessageForStateDetails = function () {
+	var state = this.upload.getState(),
+		stateDetails = this.upload.getStateDetails(),
+		warnings = stateDetails.upload && stateDetails.upload.warnings;
+
+	if ( state === mw.Upload.State.WARNING ) {
+		if ( warnings.exists !== undefined ) {
+			return $.Deferred().resolve( new OO.ui.Error(
+				$( '<p>' ).msg( 'fileexists', 'File:' + warnings.exists ),
+				{ recoverable: true, warning: true }
+			) );
+		} else if ( warnings[ 'duplicate-archive' ] !== undefined ) {
+			return $.Deferred().resolve( new OO.ui.Error(
+				$( '<p>' ).msg( 'file-deleted-duplicate', 'File:' + warnings[ 'duplicate-archive' ] ),
+				{ recoverable: true, warning: true }
+			) );
+		} else if ( warnings[ 'was-deleted' ] !== undefined ) {
+			return $.Deferred().resolve( new OO.ui.Error(
+				$( '<p>' ).msg( 'filewasdeleted', 'File:' + warnings[ 'was-deleted' ] ),
+				{ recoverable: true,warning: true }
+			) );
+		}
+	}
+	return bs.vec.ui.ForeignStructuredUpload.BookletLayout.parent.prototype.getErrorMessageForStateDetails.call( this );
 };
