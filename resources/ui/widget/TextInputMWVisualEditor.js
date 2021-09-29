9ec07a0fbddb1f1bf29a898c3c00e1803c49d226
pwirth <wirth@hallowelt.biz>
2021-09-29 12:56:29 +0200

    Fix error on getValue but the VE was not initialized yet...
    ...this usualy happens when the default value is preloaded and the user
    clicks cancel without ever setting the focus is into the textfield, the
    VisualEditor keeps track of
    
    ERM:17754
    [3.2.x]
    
    Change-Id: I0cb3333cf2333f4b2ddf2a2afcc1cce41e05d42e

--- resources/ui/widget/TextInputMWVisualEditor.js
+++ resources/ui/widget/TextInputMWVisualEditor.js
@@ -30,6 +30,9 @@ bs.ui.widget.TextInputMWVisualEditor.prototype.onFocus = function() {
  * @returns {string}
  */
 bs.ui.widget.TextInputMWVisualEditor.prototype.getValue = function() {
+	if ( !this.visualEditor ) {
+		return this.currentValue;
+	}
 	var promise = this.visualEditor.getWikiText();
 	var me = this;
 	promise.done( function ( value ) {
