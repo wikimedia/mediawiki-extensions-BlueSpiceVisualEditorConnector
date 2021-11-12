959033eb603691b66a778227498e283a0dfa2219
d.savuljesku <savuljesku@hallowelt.com>
2021-11-12 09:51:02 +0100

    Add support for pasting mailto links
    This is only a half-decent solution, as it will only handle when pasting only the link,
    not when the link is included in the paste content.
    I could not find any way to add support for this, and as other datatransferhandlers also
    do not support it, im thinking its not possible
    
    [3.2.x]
    
    ERM 26477
    
    Change-Id: Ied095c4b14f673a92fe19b9afe1744bb63179c00

---
+++ resources/ext.bluespice.visualEditorConnector.overrides.classes/dataTransferHandler/MailtoHTMLStringTransferHandler.js
@@ -0,0 +1,62 @@
+bs.vec.ui.MailtoHTMLStringTransferHandler = function() {
+	// Parent constructor
+	bs.vec.ui.MailtoHTMLStringTransferHandler.super.apply( this, arguments );
+};
+
+/* Inheritance */
+
+OO.inheritClass( bs.vec.ui.MailtoHTMLStringTransferHandler, ve.ui.HTMLStringTransferHandler );
+
+/* Static properties */
+bs.vec.ui.MailtoHTMLStringTransferHandler.static.name = 'mailtoProtocol';
+
+bs.vec.ui.MailtoHTMLStringTransferHandler.static.types = ['text/plain', 'text/html', 'application/xhtml+xml'];
+
+bs.vec.ui.MailtoHTMLStringTransferHandler.static.handlesPaste = true;
+
+/**
+ * RegExp matching input containing "file:/" or "File:/".
+ * @property {RegExp}
+ * @private
+ */
+bs.vec.ui.MailtoHTMLStringTransferHandler.static.urlRegExp = /^<a.*href=\"(mailto:.*)\".*>(.*)<\/a>$/i; // Initialized below
+
+/**
+ * Checks input item with urlRegExp expression
+ *
+ * @param {ve.ui.DataTransferItem} item
+ * @return bool Whether item matches against urlRegExp
+ */
+bs.vec.ui.MailtoHTMLStringTransferHandler.static.matchFunction = function ( item ) {
+	if ( bs.vec.ui.MailtoHTMLStringTransferHandler.static.types.indexOf( item.type ) >= 0 ) {
+		return bs.vec.ui.MailtoHTMLStringTransferHandler.static.urlRegExp.test(
+			item.getAsString()
+		);
+	}
+};
+
+/**
+ * @inheritdoc
+ */
+bs.vec.ui.MailtoHTMLStringTransferHandler.prototype.process = function () {
+	var text = this.item.getAsString().trim(),
+		matches = text.match( /<a.*href=\"(mailto:.*)\".*>(.*)<\/a>/ );
+	var url = matches[1],
+		title = matches[2];
+
+	var newLinkAnnotation = new ve.dm.MWExternalLinkAnnotation( {
+		type: 'link/mwExternal',
+		attributes: { href: url }
+	} );
+
+	var store = this.surface.getModel().getDocument().getStore();
+	var annotationSet = new ve.dm.AnnotationSet( store, store.hashAll( [
+		newLinkAnnotation
+	] ) );
+	var result = [ title ];
+	ve.dm.Document.static.addAnnotationsToData( result , annotationSet );
+	this.resolve( result );
+};
+
+/* Registration */
+ve.ui.dataTransferHandlerFactory.register( bs.vec.ui.MailtoHTMLStringTransferHandler );
