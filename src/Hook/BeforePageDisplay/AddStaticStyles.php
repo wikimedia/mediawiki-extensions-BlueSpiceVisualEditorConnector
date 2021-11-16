ac095ab95ad52ce7e554e83b1249543a52c6508a
dvogel <daniel.vogel@hallowelt.com>
2021-11-16 12:21:03 +0100

    Add common table styles for selectable tables in VE tables property dialog
    [4.1.x]
    
    ERM26466
    
    Change-Id: Iba31519ff98fce565e86553a1df7c2324c5e995a
    (cherry picked from commit 34af092f3c77bb654677c5b6067cd0bb30cd3aef)

--- src/Hook/BeforePageDisplay/AddStaticStyles.php
+++ src/Hook/BeforePageDisplay/AddStaticStyles.php
@@ -9,6 +9,8 @@ class AddStaticStyles extends BeforePageDisplay {
 		// These are the styles used in view mode, for text/background color classes set by
 		// the inspectors in VisualEditor - should be loaded on every page load
 		$this->out->addModuleStyles( 'ext.bluespice.visualEditorConnector.colors' );
+		// These are common styles for tables like contenttable, cuscosky, ...
+		$this->out->addModuleStyles( 'ext.bluespice.visualEditorConnector.tables' );
 		return true;
 	}
 }
