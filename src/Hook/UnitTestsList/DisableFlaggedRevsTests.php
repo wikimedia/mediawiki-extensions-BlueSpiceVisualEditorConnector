938d507cab33946649d51a61720fa561e4ecb3fe
Markus Glaser <glaser@hallowelt.com>
2022-09-30 12:01:01 +0200

    Disable FlaggedRevs unit tests
    These tests are not related to VisualEditorConnector. Also, they are
    currently broken with our test template.
    
    [4.x]
    
    ERM 30005
    
    Change-Id: Iaf90a660e558e513d25d63e82b4148df4fe4a804

---
+++ src/Hook/UnitTestsList/DisableFlaggedRevsTests.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ * This removes FlaggedRevs from the list of unit tests to be executed. FlaggedRevs is included by
+ * VisualEditor, which is a dependency of VisualEditorConnector. While the tests in FlaggedRevs
+ * repository pass for master and REL1_39, they fail, when the tests are executed within the context
+ * of VisualEditorConnector. The most likely reason for this is, that FlaggedRevs uses a different
+ * test template in gerrit/Jenkins. The fails are not related to VisualEditorConnector.
+ *
+ * Our best option for now is to remove the tests.
+ */
+
+namespace BlueSpice\VisualEditorConnector\Hook\UnitTestsList;
+
+use BlueSpice\Hook;
+
+class DisableFlaggedRevsTests extends Hook {
+
+	/**
+	 *
+	 * @var array
+	 */
+	protected $paths;
+
+	/**
+	 *
+	 * @param array &$paths A list of paths to extension unit test folders
+	 * @return mixed
+	 */
+	public static function callback( &$paths ) {
+		$className = static::class;
+		$hookHandler = new $className(
+			null,
+			null,
+			$paths
+		);
+		return $hookHandler->process();
+	}
+
+	/**
+	 *
+	 * @param IContextSource $context
+	 * @param Config $config
+	 * @param array &$paths A list of paths to extension unit test folders
+	 * @return bool
+	 */
+	public function __construct( $context, $config, &$paths ) {
+		parent::__construct( $context, $config );
+		$this->paths = &$paths;
+	}
+
+	protected function doProcess() {
+		foreach ( $this->paths as $key => $path ) {
+			if ( str_contains( $path, 'FlaggedRevs/tests/phpunit' ) ) {
+				unset( $this->paths[$key] );
+			}
+		}
+		return $this->paths;
+	}
+}
