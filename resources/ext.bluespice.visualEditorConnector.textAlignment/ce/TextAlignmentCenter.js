0183b2e0cf4fb73ef1da5779b3083e1412297620
d.savuljesku <d.savuljesku@gmail.com>
2022-10-13 16:14:27 +0200

    Add functionality
    - Adding line breaks
    - Center-align text
    - Indent paragraphs
    
    Depends-On: I5c534a930d894c853c465fd3cf65a0f872892b4b
    
    [4.2.x]
    
    ERM 30012
    
    Change-Id: I668b364bc31c8f8accdf5ee9619b20c434fc3854

---
+++ resources/ext.bluespice.visualEditorConnector.textAlignment/ce/TextAlignmentCenter.js
@@ -0,0 +1,40 @@
+bs.util.registerNamespace( 'bs.vec.ce' );
+
+bs.vec.ce.TextAlignmentCenter = function() {
+	// Parent constructor
+	bs.vec.ce.TextAlignmentCenter.super.apply( this,  arguments );
+
+	// DOM changes
+	this.$element
+		.addClass( 've-ce-textStyleAnnotation' )
+		.addClass( 've-ce-alignCenter' )
+		.addClass( 've-ce-bidi-isolate' )
+		.prop( {
+			title: this.constructor.static.getDescription( this.model )
+		} );
+
+	this.$element.css( 'text-align', 'center' );
+};
+
+/* Inheritance */
+
+OO.inheritClass( bs.vec.ce.TextAlignmentCenter, ve.ce.Annotation );
+
+/* Static Properties */
+
+bs.vec.ce.TextAlignmentCenter.static.name = 'textStyle/align-center';
+
+bs.vec.ce.TextAlignmentCenter.static.tagName = 'p';
+
+/* Static Methods */
+
+/**
+ * @inheritdoc
+ */
+bs.vec.ce.TextAlignmentCenter.static.getDescription = function ( model ) {
+	return OO.ui.deferMsg( "bs-visualeditorconnector-align-center-annotation-desc" );
+};
+
+/* Registration */
+
+ve.ce.annotationFactory.register( bs.vec.ce.TextAlignmentCenter );
---
+++ resources/ext.bluespice.visualEditorConnector.textAlignment/dm/TextAlignmentCenter.js
@@ -0,0 +1,34 @@
+bs.util.registerNamespace( 'bs.vec.dm' );
+
+bs.vec.dm.TextAlignmentCenter = function( element, store ) {
+	// Parent constructor
+	bs.vec.dm.TextAlignmentCenter.super.apply( this, [ element, store ] );
+};
+
+/* Inheritance */
+
+OO.inheritClass( bs.vec.dm.TextAlignmentCenter, ve.dm.Annotation );
+
+/* Static Properties */
+
+bs.vec.dm.TextAlignmentCenter.static.name = 'textStyle/align-center';
+
+bs.vec.dm.TextAlignmentCenter.static.matchTagNames = [ 'span' ];
+bs.vec.dm.TextAlignmentCenter.static.matchFunction = function ( domElement ) {
+	return domElement.style.textAlign === 'center';
+};
+
+bs.vec.dm.TextAlignmentCenter.static.applyToAppendedContent = true;
+
+
+bs.vec.dm.TextAlignmentCenter.static.toDomElements = function ( dataElement, doc ) {
+	// Cannot wrap in <p> because it will be removed by the sanitizer
+	var el = doc.createElement( 'span' );
+	el.style.textAlign = 'center';
+	el.style.display = 'block';
+	return [ el ];
+};
+
+/* Registration */
+
+ve.dm.modelRegistry.register( bs.vec.dm.TextAlignmentCenter );
