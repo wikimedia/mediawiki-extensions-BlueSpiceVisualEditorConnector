b15b39fcce2155a782e3f441027bc247841f72a6
d.savuljesku <d.savuljesku@gmail.com>
2023-01-11 13:38:26 +0100

    Allow specifying VE Standalone plugins
    Not all plugins are suitable for SA mode, so its a separate configuration.
    To keep some things generic, added to `VisualEditor` attribute, eventhough it
    wont be evaluated with BSVEC
    
    [4.2.x]
    
    ERM 30518
    
    Change-Id: Ic161a52800e6fe14359cc9a5245e22a7e80578cc

---
+++ src/ClientConfig/StandalonePluginModules.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace BlueSpice\VisualEditorConnector\ClientConfig;
+
+class StandalonePluginModules extends PluginModules {
+	/**
+	 * @return string
+	 */
+	protected function getAttribute() {
+		return 'VisualEditorStandalonePluginModules';
+	}
+}
