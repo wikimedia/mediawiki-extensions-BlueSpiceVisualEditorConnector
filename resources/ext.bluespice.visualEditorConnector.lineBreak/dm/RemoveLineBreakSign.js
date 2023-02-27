9c533765cd611e509e5869e925c13f66e36238be
pavloyashchenko <pyashchenko@hallowelt.com>
2023-02-27 16:54:21 +0100

    Delete linebreak sign ('↵') from line in VE-mode
    ERM 31594
    
    [4.2.x]
    
    Change-Id: Ia2be9deb5c184ba60ecc274c173b3eea219cde55

---
+++ resources/ext.bluespice.visualEditorConnector.lineBreak/dm/RemoveLineBreakSign.js
@@ -0,0 +1,18 @@
+bs.util.registerNamespace('bs.vec.dm');
+
+bs.vec.dm.RemoveLineBreakSign = function () {
+	// Parent constructor
+	bs.vec.dm.RemoveLineBreakSign.super.apply(this, arguments);
+};
+
+/* Inheritance */
+OO.inheritClass(bs.vec.dm.RemoveLineBreakSign, ve.dm.BreakNode);
+
+bs.vec.dm.RemoveLineBreakSign.static.toDataElement = function (domElement, doc) {
+	if (domElement[0].nextSibling) {
+		domElement[0].nextSibling.textContent = domElement[0].nextSibling.textContent.trim();
+	}
+};
+
+/* Registration */
+ve.dm.modelRegistry.register(bs.vec.dm.RemoveLineBreakSign);
