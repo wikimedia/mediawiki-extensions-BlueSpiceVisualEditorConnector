b02016188e73b8251dc0f816887ddb16af68ff45
d.savuljesku <d.savuljesku@gmail.com>
2023-04-28 16:26:27 +0200

    Make line break work again
    [4.2.x]
    
    ERM31604
    
    Change-Id: I8a14dabfb1163c5e9074808f7ec9c016981bdd94

---
+++ resources/ext.bluespice.visualEditorConnector.lineBreak/dm/DeprecatedExplicitLineBreakNode.js
@@ -0,0 +1,26 @@
+bs.util.registerNamespace( 'bs.vec.dm' );
+
+bs.vec.dm.DeprecatedExplicitLineBreak = function() {
+	// Parent InternalFileLinkAnnotation
+	bs.vec.dm.DeprecatedExplicitLineBreak.super.apply( this, arguments );
+};
+
+OO.inheritClass( bs.vec.dm.DeprecatedExplicitLineBreak, bs.vec.dm.ExplicitLineBreak );
+
+bs.vec.dm.DeprecatedExplicitLineBreak.static.name = 'deprecatedExplicitBreak';
+bs.vec.dm.DeprecatedExplicitLineBreak.static.matchTagNames = [ 'span' ];
+bs.vec.dm.DeprecatedExplicitLineBreak.static.matchFunction = function( element ) {
+	return element.classList.contains( 'bs-vec-line-break' );
+};
+
+bs.vec.dm.DeprecatedExplicitLineBreak.static.toDomElements = function ( dataElement, doc ) {
+	var nodes = bs.vec.dm.DeprecatedExplicitLineBreak.parent.static.toDomElements.call( this, dataElement, doc );
+
+	// Doesnt seem possible to remove `class` completely. If we do, it is just re-added later on somewhere
+	// By removing class name, we at least get `class=""` instead of `class="bs-vec-line-break"`
+	nodes[0].className = '';
+	return nodes;
+};
+
+/* Registration */
+ve.dm.modelRegistry.register( bs.vec.dm.DeprecatedExplicitLineBreak );
