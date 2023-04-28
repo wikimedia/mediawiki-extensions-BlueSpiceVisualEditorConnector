8559b3964cc948c8880c66a34ce78cc468bfed95
d.savuljesku <d.savuljesku@gmail.com>
2023-04-28 16:26:27 +0200

    Make line break work again
    [4.2.x]
    
    ERM31604
    
    Change-Id: I8a14dabfb1163c5e9074808f7ec9c016981bdd94

---
+++ resources/ext.bluespice.visualEditorConnector.lineBreak/ce/ExplicitLineBreak.js
@@ -0,0 +1,18 @@
+bs.util.registerNamespace( 'bs.vec.ce' );
+
+bs.vec.ce.ExplicitLineBreak = function() {
+	// Parent InternalFileLinkAnnotation
+	bs.vec.ce.ExplicitLineBreak.super.apply( this, arguments );
+	this.$element.addClass( 'bs-vec-line-break' );
+};
+
+/* Inheritance */
+
+OO.inheritClass( bs.vec.ce.ExplicitLineBreak, ve.ce.BreakNode );
+
+/* Static Properties */
+
+bs.vec.ce.ExplicitLineBreak.static.name = 'explicitBreak';
+
+/* Registration */
+ve.ce.nodeFactory.register( bs.vec.ce.ExplicitLineBreak );
---
+++ resources/ext.bluespice.visualEditorConnector.lineBreak/dm/ExplicitLineBreakNode.js
@@ -0,0 +1,25 @@
+bs.util.registerNamespace( 'bs.vec.dm' );
+
+bs.vec.dm.ExplicitLineBreak = function() {
+	// Parent InternalFileLinkAnnotation
+	bs.vec.dm.ExplicitLineBreak.super.apply( this, arguments );
+};
+
+OO.inheritClass( bs.vec.dm.ExplicitLineBreak, ve.dm.BreakNode );
+
+bs.vec.dm.ExplicitLineBreak.static.name = 'explicitBreak';
+bs.vec.dm.ExplicitLineBreak.static.matchFunction = function( element ) {
+	// We never want to match <br> nodes, that is handled by VE by default
+	// if <br> comes from wikitext. This node is only for inserting from Visual mode
+	return false;
+};
+
+bs.vec.dm.ExplicitLineBreak.static.toDomElements = function ( dataElement, doc ) {
+	var node = doc.createElement( 'br' );
+	// Add this explicitly to "mark" the break as coming from HTML, so that it won't get stripped by Parsoid
+	node.setAttribute( 'data-parsoid', JSON.stringify( { stx: 'html' } ) );
+	return [ node ];
+};
+
+/* Registration */
+ve.dm.modelRegistry.register( bs.vec.dm.ExplicitLineBreak );
