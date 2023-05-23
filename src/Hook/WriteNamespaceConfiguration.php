451911e9abca10b34522634912e250b098535a1f
d.savuljesku <d.savuljesku@gmail.com>
2023-05-23 16:20:33 +0200

    Adapt to changes in NamespaceManager DynamicConfig
    Depends-On: I53d16ccc5ca447635d39a143edf3bcbe2f94da5c
    
    [4.x]
    
    ERM31211
    
    Change-Id: I3f14bea509ef1f8ae03cd44461fdbb51fca8580c
    (cherry picked from commit b575a85f0efd4f2890587ab2e51cb72bea094421)
    (cherry picked from commit 80a4a31aaa741cf4994eca466d5d442e08264c6b)

---
+++ src/Hook/WriteNamespaceConfiguration.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace BlueSpice\VisualEditorConnector\Hook;
+
+use BlueSpice\NamespaceManager\Hook\NamespaceManagerBeforePersistSettingsHook;
+
+class WriteNamespaceConfiguration implements NamespaceManagerBeforePersistSettingsHook {
+
+	/**
+	 * @inheritDoc
+	 */
+	public function onNamespaceManagerBeforePersistSettings(
+		array &$configuration, int $id, array $definition, array $mwGlobals
+	): void {
+		$enabledNamespaces = $mwGlobals['wgVisualEditorAvailableNamespaces'] ?? [];
+		$currentlyActivated = in_array( $id, $enabledNamespaces );
+
+		$explicitlyDeactivated = false;
+		if ( isset( $definition['visualeditor'] ) && $definition['visualeditor'] === false ) {
+			$explicitlyDeactivated = true;
+		}
+
+		$explicitlyActivated = false;
+		if ( isset( $definition['visualeditor'] ) && $definition['visualeditor'] === true ) {
+			$explicitlyActivated = true;
+		}
+
+		if ( ( $currentlyActivated && !$explicitlyDeactivated ) || $explicitlyActivated ) {
+			$configuration['wgVisualEditorAvailableNamespaces'][$id] = true;
+		} else {
+			$configuration['wgVisualEditorAvailableNamespaces'][$id] = false;
+		}
+	}
+}
