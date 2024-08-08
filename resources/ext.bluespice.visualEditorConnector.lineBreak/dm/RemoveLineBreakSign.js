1ab3d605eee2ecbf58f420f572bbd2f4b45d521f
hslater <hamish.slater@hallowelt.com>
2024-08-08 13:58:48 +1200

    Reimplement RemoveLineBreakSign to handle line breaks consistently
    Ensures consistent preservation of document structure
    toDataElement: Replace newline characters with zero-width spaces
    toDomElements: Restore newline characters from zero-width spaces
    
    ERM38227
    
    [REL1_39, REL1_39-4.5.x, master]
    
    Change-Id: I0e5e9f2b2d0569c6e2fa3fc31469398ff6f12430
    (cherry picked from commit e6d8800b16eeeaa101851fbb7f0be6ce0d6da0be)

--- resources/ext.bluespice.visualEditorConnector.lineBreak/dm/RemoveLineBreakSign.js
+++ resources/ext.bluespice.visualEditorConnector.lineBreak/dm/RemoveLineBreakSign.js
@@ -1,21 +1,56 @@
-bs.util.registerNamespace('bs.vec.dm');
+bs.util.registerNamespace( 'bs.vec.dm' );
 
 bs.vec.dm.RemoveLineBreakSign = function () {
 	// Parent constructor
-	bs.vec.dm.RemoveLineBreakSign.super.apply(this, arguments);
+	bs.vec.dm.RemoveLineBreakSign.super.apply( this, arguments );
 };
 
 /* Inheritance */
-OO.inheritClass(bs.vec.dm.RemoveLineBreakSign, ve.dm.BreakNode);
+OO.inheritClass( bs.vec.dm.RemoveLineBreakSign, ve.dm.BreakNode );
 
-bs.vec.dm.RemoveLineBreakSign.static.toDataElement = function (domElement, doc) {
-	if (domElement[0].nextSibling) {
-		var prevSibling = domElement[0].previousSibling;
-		if ( prevSibling && prevSibling.textContent === "\n" ) {
-			domElement[0].nextSibling.textContent = domElement[0].nextSibling.textContent.trim();
+bs.vec.dm.RemoveLineBreakSign.static.toDataElement = function ( domElement, doc ) {
+	try {
+		const nextSibling = domElement[ 0 ].nextSibling;
+
+		if ( nextSibling && nextSibling.nodeType === Node.TEXT_NODE ) {
+			const textContent = nextSibling.textContent;
+
+			if ( textContent.startsWith( '\n' ) ) {
+				// Replace newline with zero-width space
+				nextSibling.textContent = '\u200B' + textContent.slice( 1 );
+			}
 		}
+	} catch ( error ) {
+		console.error( 'BSVEC RemoveLineBreakSign error: ' + error ); // eslint-disable-line no-console
 	}
+
+	// Parent method
+	return bs.vec.dm.RemoveLineBreakSign.parent.static.toDataElement.call( this, domElement, doc );
+};
+
+bs.vec.dm.RemoveLineBreakSign.static.toDomElements = function ( dataElement, doc ) {
+	try {
+		const brElements = doc.body.getElementsByTagName( 'br' );
+
+		Array.from( brElements ).forEach( ( brElement ) => { // eslint-disable-line compat/compat
+			const nextSibling = brElement.nextSibling;
+
+			if ( nextSibling && nextSibling.nodeType === Node.TEXT_NODE ) {
+				const textContent = nextSibling.textContent;
+
+				if ( textContent.startsWith( '\u200B' ) ) {
+					// Replace zero-width space with newline
+					nextSibling.textContent = '\n' + textContent.slice( 1 );
+				}
+			}
+		} );
+	} catch ( error ) {
+		console.error( 'BSVEC RemoveLineBreakSign error: ' + error ); // eslint-disable-line no-console
+	}
+
+	// Parent method
+	return bs.vec.dm.RemoveLineBreakSign.parent.static.toDomElements.call( this, dataElement, doc );
 };
 
 /* Registration */
-ve.dm.modelRegistry.register(bs.vec.dm.RemoveLineBreakSign);
+ve.dm.modelRegistry.register( bs.vec.dm.RemoveLineBreakSign );
