2e3387aabb5f0ec2503933b6dace2984c8078ee1
d.savuljesku <d.savuljesku@gmail.com>
2025-03-24 14:57:13 +0100

    Add file link tab
    This is moved (and fixed in the process) from BSInsertLink, as other functionality
    from that extension is to be removed
    
    [5.x]
    
    ERM40093
    
    Change-Id: I4b8467fee8781cc1d8b742752100acf1d397c7e6
    (cherry picked from commit 3eca302fe5732d90aa81d108a16de22b60a19a64)

---
+++ resources/ext.bluespice.visualEditorConnector.fileLink/FileLink.js
@@ -0,0 +1,97 @@
+bs.vec.registerComponentPlugin(
+	bs.vec.components.LINK_ANNOTATION_INSPECTOR,
+	function( component ) {
+		component.linkTypeIndex.addTabPanels( [
+			new OO.ui.TabPanelLayout( 'file', {
+				label: ve.msg( 'bs-visualeditorconnector-tab-file' ),
+				expanded: false,
+				scrollable: false,
+				padded: true
+			} )
+		] );
+
+		component.localFileSystemAnnotationInput =
+			new bs.vec.ui.MWFileLinkAnnotationWidget();
+
+		component.linkTypeIndex.getTabPanel( 'file' ).$element.append(
+			component.localFileSystemAnnotationInput.$element
+		);
+		component.localFileSystemAnnotationInput.connect( this, { change: function() {
+			this.updateActions();
+		}.bind( component ) } );
+
+		component.localFileSystemAnnotationInput.getInternalFilePicker().results.connect( component, {
+			add: function () {
+				this.updateSize();
+			}
+		} );
+	}
+);
+
+// FIXME: Not exactly cool, would be nice to have a better way of overriding, so that plugins
+// would not only be called on initialize, but on every function from prototype
+ve.ui.MWLinkAnnotationInspector.prototype.onLinkTypeIndexSet = function () {
+	var text = this.annotationInput.getTextInputWidget().getValue(),
+		end = text.length,
+		isExternal = this.isExternal() || this.linkTypeIndex.getCurrentTabPanelName() === 'file',
+		inputHasProtocol = ve.init.platform.getExternalLinkUrlProtocolsRegExp().test( text );
+
+	if ( this.linkTypeIndex.getCurrentTabPanelName() === 'file' ) {
+		this.annotationInput = this.localFileSystemAnnotationInput;
+	} else {
+		this.annotationInput = isExternal ? this.externalAnnotationInput : this.internalAnnotationInput;
+	}
+
+	this.updateSize();
+
+	// If the user manually switches to internal links with an external link in the input, remember this
+	if ( !isExternal && inputHasProtocol ) {
+		this.allowProtocolInInternal = true;
+	}
+
+	this.annotationInput.getTextInputWidget().setValue( text ).focus();
+	if ( this.linkTypeIndex.getCurrentTabPanelName() === 'file' ) {
+		this.annotationInput.internalFilePicker.getQuery().setValue( text ).focus();
+	}
+	// Select entire link when switching, for ease of replacing entire contents.
+	// Most common case:
+	// 1. Inspector opened, internal-link shown with the selected-word prefilled
+	// 2. User clicks external link tab (unnecessary, because we'd auto-switch, but the user doesn't know that)
+	// 3. User pastes a link, intending to replace the existing prefilled link
+	this.annotationInput.getTextInputWidget().$input[ 0 ].setSelectionRange( 0, end );
+	// Focusing a TextInputWidget normally unsets validity. However, because
+	// we're kind of pretending this is the same input, just in a different
+	// mode, it doesn't make sense to the user that the focus behavior occurs.
+	this.annotationInput.getTextInputWidget().setValidityFlag();
+
+	this.updateActions();
+};
+
+const getInsertionText = ve.ui.LinkAnnotationInspector.prototype.getInsertionText;
+ve.ui.MWLinkAnnotationInspector.prototype.getInsertionText = function () {
+	var annotation = this.annotationInput.getAnnotation();
+	if ( !( annotation instanceof bs.vec.dm.InternalFileLinkAnnotation ) ) {
+		return getInsertionText.call( this );
+	}
+	return bs.vec.ui.MWFileLinkAnnotationWidget.static.getTextFromAnnotation( annotation );
+};
+
+var updateAction = ve.ui.LinkAnnotationInspector.prototype.updateActions;
+ve.ui.LinkAnnotationInspector.prototype.updateActions = function () {
+	var isValid = false,
+		inspector = this,
+		annotation = this.annotationInput.getAnnotation();
+
+	if ( !( annotation instanceof bs.vec.dm.InternalFileLinkAnnotation ) ) {
+		return updateAction.call( this );
+	}
+
+	this.annotationInput.getInternalFilePicker().query.getValidity()
+		.then( function () { isValid = true; } )
+		.always( function () {
+			isValid = isValid && !!annotation;
+			inspector.actions.forEach( { actions: [ 'done', 'insert' ] }, function ( action ) {
+				action.setDisabled( !isValid );
+			} );
+		} );
+};
---
+++ resources/ext.bluespice.visualEditorConnector.fileLink/ui/MWFileLinkAnnotationWidget.js
@@ -0,0 +1,86 @@
+bs.util.registerNamespace( 'bs.vec.ui' );
+
+bs.vec.ui.MWFileLinkAnnotationWidget = function() {
+	bs.vec.ui.MWFileLinkAnnotationWidget.super.apply( this, arguments );
+	this.addOtherTools();
+	this.input.$element.hide();
+};
+
+OO.inheritClass( bs.vec.ui.MWFileLinkAnnotationWidget, ve.ui.MWExternalLinkAnnotationWidget );
+
+bs.vec.ui.MWFileLinkAnnotationWidget.prototype.onTextChange = function ( value ) {
+	if ( this.isExternal ) {
+		return bs.vec.ui.MWFileLinkAnnotationWidget.parent.prototype.onTextChange.call( this, value );
+	}
+
+	this.internalFilePicker.query.getValidity()
+		.done( function () {
+			this.setAnnotation( this.constructor.static.getAnnotationFromText( value, true ), true );
+		}.bind( this ) )
+		.fail( function () {
+			this.setAnnotation( null, true );
+		}.bind( this ) );
+};
+
+
+bs.vec.ui.MWFileLinkAnnotationWidget.prototype.addOtherTools = function () {
+	this.internalFilePicker = new mw.widgets.TitleSearchWidget( {
+		icon: 'search',
+		showRedlink: true,
+		namespace: bs.ns.NS_FILE,
+		excludeCurrentPage: true,
+		showImages: mw.config.get( 'wgVisualEditor' ).usePageImages,
+		showDescriptions: mw.config.get( 'wgVisualEditor' ).usePageDescriptions,
+		cache: ve.init.platform.linkCache
+	} );
+	this.internalFilePicker.$element.prepend( this.internalFilePicker.$query );
+	this.internalFilePicker.query.connect( this, {
+		change: 'onTextChange'
+	} );
+
+	this.$element.append( new OO.ui.FieldsetLayout( {
+		items: [
+			this.internalFilePicker
+		]
+	} ).$element );
+};
+
+bs.vec.ui.MWFileLinkAnnotationWidget.prototype.getInternalFilePicker = function () {
+	return this.internalFilePicker;
+};
+
+bs.vec.ui.MWFileLinkAnnotationWidget.static.getAnnotationFromText = function ( value ) {
+	value = value.trim();
+
+	// Keep annotation in sync with value
+	if ( value === '' ) {
+		return null;
+	} else {
+		var title = mw.Title.makeTitle( bs.ns.NS_FILE, value );
+		return bs.vec.dm.InternalFileLinkAnnotation.static.newFromTitle( title );
+	}
+};
+
+bs.vec.ui.MWFileLinkAnnotationWidget.prototype.setAnnotation = function ( annotation, fromText ) {
+	bs.vec.ui.MWFileLinkAnnotationWidget.parent.prototype.setAnnotation.call( this, annotation, fromText );
+	if ( !this.annotation )  {
+		return;
+	}
+	if ( !fromText ) {
+		this.internalFilePicker.query.setValue( this.constructor.static.getTextFromAnnotation( this.annotation ) );
+	}
+
+	return this;
+};
+
+bs.vec.ui.MWFileLinkAnnotationWidget.static.getTextFromAnnotation = function ( annotation ) {
+	if ( !annotation ) {
+		return '';
+	}
+	var type = annotation.name;
+	if ( type === 'link/internalFile' ) {
+		return annotation.element.attributes.normalizedTitle;
+	} else {
+		return '';
+	}
+};
