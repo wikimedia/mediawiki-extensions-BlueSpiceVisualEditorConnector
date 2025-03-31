1ffe06653d58b5f49bce3cc00658aa796967799b
d.savuljesku <d.savuljesku@gmail.com>
2025-03-31 11:58:42 +0200

    Allow LinkInspector plugins to override methods
    [neo]
    
    ERM37086
    
    Change-Id: Ia06d833a14376b74f875501238bf380aa8cbab1f

--- resources/ext.bluespice.visualEditorConnector.overrides.classes/MWLinkAnnotationInspector.js
+++ resources/ext.bluespice.visualEditorConnector.overrides.classes/MWLinkAnnotationInspector.js
@@ -1,6 +1,7 @@
 bs.util.registerNamespace( 'bs.vec.ui' );
 
 bs.vec.ui.MWLinkAnnotationInspector = function BsVecUiMWLinkAnnotationInspector( config ) {
+	this.pluginMethods = {};
 	bs.vec.ui.MWLinkAnnotationInspector.super.call( this, ve.extendObject( { padded: false }, config ) );
 };
 
@@ -18,6 +19,33 @@ bs.vec.ui.MWLinkAnnotationInspector.prototype.runComponentPlugins = function () 
 
 	for ( let i = 0; i < pluginCallbacks.length; i++ ) {
 		const callback = pluginCallbacks[ i ];
-		callback( this );
+		const pluginMethods = callback( this ) || {};
+		for ( const method in pluginMethods ) {
+			if ( !this.pluginMethods.hasOwnProperty( method ) ) {
+				this.pluginMethods[ method ] = [];
+			}
+			this.pluginMethods[ method ].push( pluginMethods[ method ] );
+		}
 	}
+};
+
+bs.vec.ui.MWLinkAnnotationInspector.prototype.updateActions = function () {
+	const pluginMethods = this.pluginMethods.updateActions || [];
+	for ( let i = 0; i < pluginMethods.length; i++ ) {
+		if ( pluginMethods[ i ].call( this ) === false ) {
+			return;
+		}
+	}
+	bs.vec.ui.MWLinkAnnotationInspector.super.prototype.updateActions.call( this );
+};
+
+bs.vec.ui.MWLinkAnnotationInspector.prototype.getInsertionText = function () {
+	const pluginMethods = this.pluginMethods.getInsertionText || [];
+	for ( let i = 0; i < pluginMethods.length; i++ ) {
+		const res = pluginMethods[ i ].call( this );
+		if ( res !== null ) {
+			return res;
+		}
+	}
+	return bs.vec.ui.MWLinkAnnotationInspector.super.prototype.getInsertionText.call( this );
 };
